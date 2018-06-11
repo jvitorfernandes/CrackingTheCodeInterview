05ba22ec46c2eeb5ed5f376250b2bf9256895811
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2018-06-11 11:42:42 -0500

    Trees: Is This a Binary Search Tree?

---
+++ DataStructures/Trees: Is This a Binary Search Tree?.py
@@ -0,0 +1,18 @@
+""" Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+"""
+later = []
+def checkBST(root, min=-1, max=10001):
+    
+    if root is None:
+        return True
+    
+    elif(root.data < min or root.data > max):
+        return False
+    
+    return checkBST(root.left, min, root.data-1) and checkBST(root.right, root.data+1, max)
+        
